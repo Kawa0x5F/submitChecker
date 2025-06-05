3a60b3bb2ce69ec201cb996a4c7c34cf47c3ec33
Kawa_ <yuyux0910@gmail.com>
2025-06-05 16:13:00 +0900

    feature: ウィンドウの表示文字列を任意の文字列に変更 #1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
+import { invoke } from '@tauri-apps/api/core';
+
 // 文字列を表示する
 function App() {
-    return(
-        <div>
-            <p>Hello Tauri!</p>
-        </div>
+	const [greetMsg, setGreetMsg] = useState<string>();
+	const [name, setName] = useState<string>();
+
+	async function greet() {
+		setGreetMsg(await invoke("greet", { name }));
+	}
+
+	return(
+		<div className="container">
+			<h1>Welcome Tauri!</h1>
+
+			<form
+				className="row"
+				onSubmit={(e)=> {
+					e.preventDefault();
+					greet();
+				}}
+			>
+				<input
+					id="greet-input"
+					onChange={(e) => setName(e.currentTarget.value)}
+					placeholder="Enter a name..."
+				 />
+				 <button type="submit">Greet</button>
+			</form>
+
+			<p>{greetMsg}</p>
+	</div>
     )
 }
 
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,14 @@
+// リリースビルド時にWindowsで余分なコンソール画面が表示されるのを防ぐ
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
 fn main() {
-    println!("Hello World!");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
