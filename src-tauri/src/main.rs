0951765712aee1dd0d95ea331ffd04ba401c5db5
Kawa_ <yuyux0910@gmail.com>
2025-06-18 11:03:02 +0900

    feature:ダイアログを許可

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,6 +14,7 @@ fn find_folders(parent_folder_path: &str) -> Vec<String> {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_dialog::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
